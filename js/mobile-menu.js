92c36203132a2973b769f4848324c4844de9acc0
Inna Zhuravlova <inessa38@gmail.com>
2022-01-04 01:03:31 +0100

    1.4

---
+++ js/mobile-menu.js
@@ -0,0 +1,9 @@
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
+
+  menuBtnRef.addEventListener("click", () => { mobileMenuRef.classList.toggle("is-open"); });
+  
+  mobileBtnClose.addEventListener("click", () => { mobileMenuRef.classList.toggle("is-open"); });
+})();
